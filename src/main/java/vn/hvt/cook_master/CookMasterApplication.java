c6d4de42357e138704a4772270b97fbe185bfdba
tuapluctt <hoangvantunkv@gmail.com>
2024-12-09 16:14:04 +0700

    Initial commit

---
+++ src/main/java/vn/hvt/cook_master/CookMasterApplication.java
@@ -0,0 +1,13 @@
+package vn.hvt.cook_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/hvt/cook_master/CookMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hvt.cook_master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
